7be0e5b0298335238f3a9864e8934fb4857c21f3
Shristi Goyal <goyal.shristi96@gmail.com>
2017-10-09 20:41:27 +0530

    Plot

--- alexnet.py
+++ alexnet.py
@@ -6,6 +6,7 @@ from torch.autograd import Variable
 from torchvision import transforms, datasets
 import torch.nn.functional as F
 import torch.nn.init as init
+import matplotlib.pyplot as plt
 
 num_classes = 35
 	
@@ -77,56 +78,86 @@ class AlexNet(nn.Module):
 		x = self.linear(x)
 		return x
 
-alex_net = Net()
-alex_net_optimal = Net()
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(alex_net.parameters(), lr=0.01,wd = 0.0005, momentum=0.9)
-scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer,  mode='min', factor=0.1, patience=10)
+def train()
+	alex_net = Net()
+	alex_net_optimal = Net()
+	criterion = nn.CrossEntropyLoss()
+	optimizer = optim.SGD(alex_net.parameters(), lr=0.01,wd = 0.0005, momentum=0.9)
+	scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer,  mode='min', factor=0.1, patience=10)
 
-#stopping criteria parameters
-wait = 0
-best_loss = 1e15
-min_delta = 1e-5
-p = 10
-#for epoch in range(2):  # loop over the dataset multiple times
+	#stopping criteria parameters
+	wait = 0
+	best_loss = 1e15
+	min_delta = 1e-5
+	p = 10
+	#for epoch in range(2):  # loop over the dataset multiple times
 
-epoch = 0
-while epoch < p
+	epoch = 0
+	j = 0
 
-    running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
-        # get the inputs
-        inputs, labels = data
+	train_error = []
+	iter_train = []
+	while epoch < p
+		j++
+	    running_loss = 0.0
+	    for i, data in enumerate(trainloader, 0):
+	        # get the inputs
+	        inputs, labels = data
 
-        # wrap them in Variable
-        inputs, labels = Variable(inputs), Variable(labels)
+	        # wrap them in Variable
+	        inputs, labels = Variable(inputs), Variable(labels)
 
-        # zero the parameter gradients
-        optimizer.zero_grad()
+	        # zero the parameter gradients
+	        optimizer.zero_grad()
 
-        # forward + backward + optimize
-        outputs = alex_net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
+	        # forward + backward + optimize
+	        outputs = alex_net(inputs)
+	        loss = criterion(outputs, labels)
+	        loss.backward()
+	        optimizer.step()
 
-        # print statistics
-        running_loss += loss.data[0]
-        if i % 2000 == 1999:    # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
-            running_loss = 0.0
+	        # # print statistics
+	        running_loss += loss.data[0]
+	        # if i % 2000 == 1999:    # print every 2000 mini-batches
+	        #     print('[%d, %5d] loss: %.3f' %
+	        #           (epoch + 1, i + 1, running_loss / 2000))
+	        #     running_loss = 0.0
 
 
-    val_input,val_label = val_dataset
-    val_output = alex_net(val_input)
-   	val_loss = criterion(val_output,val_label)
-   	scheduler.step(val_loss)  
-    if (val_loss - best_loss) < -min_delta:
-        best_loss = val_loss
-        epoch = 0
-        alex_net_optimal.load_state_dict(alex_net.state_dict())
-    else:
-        epoch++
+	    train_error.append(running_loss/trainloader._len_())
+	    iter_train.append(j)
 
-print('Finished Training')
+	    val_input,val_label = val_dataset
+	    val_output = alex_net(val_input)
+	   	val_loss = criterion(val_output,val_label)
+	   	scheduler.step(val_loss)  
+
+	    if (val_loss - best_loss) < -min_delta:
+	        best_loss = val_loss
+	        epoch = 0
+	        alex_net_optimal.load_state_dict(alex_net.state_dict())
+	    else:
+	        epoch++
+
+	#print('Finished Training')
+	plt.plot(iter_train, train_error, label='Train')
+    plt.xlabel('Epoch')
+    plt.ylabel('Cross-Entropy Loss')
+	plt.legend()
+	
+	return alex_net_optimal
+
+def test(val_loader, model)
+	correct = 0
+	total = 0
+	for data in testloader:
+    	images, labels = data
+    	outputs = model(Variable(images))
+    	_, predicted = torch.max(outputs.data, 1)
+    	total += labels.size(0)
+    	correct += (predicted == labels).sum()
+
+	print('Accuracy of the network on the 10000 test images: %d %%' % (
+    	100 * correct / total))
+	return 100*correct/total
+
